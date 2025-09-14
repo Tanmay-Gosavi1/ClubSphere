a841057c47f763f8395e9cc324bed79d496285e8
Tanmay Gosavi <202452335@iiitvadodara.ac.in>
2025-09-14 12:59:57 +0530

    Add protected route for create/edit club

--- routes/clubRoutes.js
+++ routes/clubRoutes.js
@@ -1,7 +1,10 @@
 const { createClub, getAllClubs } = require('../controllers/clubController')
 const router = require('express').Router() 
+const { editClub } = require('../controllers/clubController')
+const { auth, authorizeRoles } = require('../middleware/authMiddleware')
 
-router.post('/createClub' , createClub)
+router.post('/createClub' , auth , authorizeRoles('admin') , createClub) // Only admin can create a club
 router.get('/allClubs' , getAllClubs)
+router.post('/editClub/:clubId' ,auth , authorizeRoles('admin') , editClub)
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const connectDB = require('./config/db.js')
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
-const cloudinary = require('./config/cloudinary.js')
 const fileUpload = require('express-fileupload')
+const cors = require('cors')
 
+const connectDB = require('./config/db.js')
+const cloudinary = require('./config/cloudinary.js')
 const authRouter = require('./routes/authRoutes.js')
 const clubRoutes = require('./routes/clubRoutes.js')
 const cloudinaryConnect = require('./config/cloudinary.js')
@@ -16,6 +17,10 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
+app.use(cors( {
+    origin: 'http://localhost:8001',
+    credentials: true,
+}))
 app.use(fileUpload({
     useTempFiles : true ,
     tempFileDir : '/tmp/'
